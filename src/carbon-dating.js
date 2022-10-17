926632ed37636fc641769054e9ffda6d1e1260b7
Svitlana <93516763+Svitlanka-K@users.noreply.github.com>
2022-10-17 15:17:59 +0300

    fix: fix test solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -18,16 +18,12 @@ const HALF_LIFE_PERIOD = 5730;
  *
  */
 function dateSample(sampleActivity) {
-if (typeof(sampleActivity) !== 'string') {return false}
-  if (sampleActivity == 'undefined') {return false}
- else if (isNaN(Number(sampleActivity))) {return false} 
- else if (Number(sampleActivity) >= 9000) {return false}
- else if (Number(sampleActivity) <= 0) {return false}
-  else {
-   let k = 0.693 / HALF_LIFE_PERIOD;
-     T = Math.log(MODERN_ACTIVITY / sampleActivity) / k;
-   return Math.ceil(T) > 0 ? Math.round(T) : false;
- } 
+let time;
+  if ((typeof(sampleActivity) !== 'string') || (isNaN(sampleActivity)) || (sampleActivity.length = 0) || (sampleActivity <= 0) || (sampleActivity > MODERN_ACTIVITY))
+  { return false;
+  } else {
+   return time = Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / (0.693 / HALF_LIFE_PERIOD));
+}
 }
 
 module.exports = {
